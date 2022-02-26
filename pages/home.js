390bc65730b9d360583aa74ca2c14f7df4044f78
Thomas <tomsclater@live.com>
2022-02-26 15:30:29 -0800

    Added Sign In Button

--- pages/home.js
+++ pages/home.js
@@ -20,6 +20,11 @@ function Home() {
                 <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
                 <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
             </div>
+            <div className="pl-4">
+              <button className="text-blue-700 font-semibold rounded-full border border-blue-700 px-5 py-1.5 transition-all hover:border-2">
+                Sign in
+              </button>
+            </div>
           </div>
       </header>
   </div>;
