b69264b80441f70ff189ddfb4a0760acbbe36cdd
Thomas <tomsclater@live.com>
2022-02-26 00:13:15 -0800

    Adding HeaderLink

--- pages/home.js
+++ pages/home.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import HeaderLink from "../components/HeaderLink";
 
 function Home() {
   return <div>
       <header>
-          <div className="relative w-32 h-20">
-              <Image src="http://www.myownpaints.com/wp-content/uploads/2022/02/Logo.svg" layout="fill" objectFit="contain" />
+          <div className="relative w-32 h-20 ">
+              <Image src="http://www.myownpaints.com/wp-content/uploads/2022/02/Logo.svg" layout="fill" 
+              objectFit="contain" />
+          </div>
+          <div className="flex items-center sm:divide-x divide-gray-300">
+            <div className="hidden sm:flex space-x-8 pr-4">
+                <HeaderLink/>
+            </div>
           </div>
       </header>
   </div>;
