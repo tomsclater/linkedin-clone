024a3702b90099c0a7806ae12081f730655aae97
Thomas <tomsclater@live.com>
2022-02-26 15:17:59 -0800

    Gray color and hover effect to header links

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,9 +1,14 @@
-function HeaderLink({ Icon, text, avatar }) {
+function HeaderLink({ Icon, text, avatar, feed }) {
   return (
-    <div className="cursor-pointer flex flex-col justify-center items-center">
-        {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" /> : <Icon />}
+    <div className={`cursor-pointer flex flex-col justify-center items-center ${
+        feed
+        ? "text-black/60 hover:text-black dark:text-white/75 dark:hover:text-white lg:-mb-1.5 space-y-1" 
+        : "text-gray-500 hover:text-gray-700"
+      }`}
+    >
+      {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" /> : <Icon />}
 
-        <h4 className="text-sm">{text}</h4>
+      <h4 className="text-sm">{text}</h4>
     </div>
   );
 }
