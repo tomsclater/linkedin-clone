e1cffd8c503d2b64d645f6909ff981367fce37dd
Thomas <tomsclater@live.com>
2022-02-25 23:50:53 -0800

    Logo added to Home

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+function Home() {
+  return <div>
+      <header>
+          <div>
+              <Image src="https://1000marcas.net/wp-content/uploads/2020/01/Logo-Linkedin.png" layout="fill" objectFit="contain" />
+          </div>
+      </header>
+  </div>;
+}
+
+export default Home;
