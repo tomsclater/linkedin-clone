5dd739260aa85113df2271e817393a26815c6f29
Thomas <tomsclater@live.com>
2022-02-26 15:11:20 -0800

    Header links added

--- components/HeaderLink.js
+++ components/HeaderLink.js
@@ -1,5 +1,11 @@
-function HeaderLink(props) {
-  return <div>{props.text}</div>;
+function HeaderLink({ Icon, text, avatar }) {
+  return (
+    <div className="cursor-pointer flex flex-col justify-center items-center">
+        {avatar ? <Icon className="!h-7 !w-7 lg:!-mb-1" /> : <Icon />}
+
+        <h4 className="text-sm">{text}</h4>
+    </div>
+  );
 }
 
 export default HeaderLink;
