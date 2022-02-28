be17483ef7373ff5bc0d0413ef6606785ca12950
Thomas <tomsclater@live.com>
2022-02-28 07:34:16 -0800

    Fixed redirect error

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,15 +2,18 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  if (req.nextUrl.pathname === "/") {
-    const session = await getToken({
-      req,
-      secret: process.env.JWT_SECRET,
-      secureCookie: process.env.NODE_ENV === "production",
-    });
-    // You could also check for any property on the session object,
-    // like role === "admin" or name === "John Doe", etc.
-    if (!session) return NextResponse.redirect("https://myownpaints.com");
-    // If user is authenticated, continue.
-  }
+const url = req.nextUrl.clone();
+url.pathname = "/home";
+if (req.nextUrl.pathname === "/") {
+const session = await getToken({
+req,
+secret: process.env.JWT_SECRET,
+secureCookie: process.env.NODE_ENV === "production",
+});
+// You could also check for any property on the session object,
+// like role === "admin" or name === "John Doe", etc.
+if (!session) return NextResponse.redirect(url);
+// If user is authenticated, continue.
 }
+}
+
