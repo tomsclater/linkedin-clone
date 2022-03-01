0e09d8197153242e6f4c29edfc756524671f0351
Thomas <tomsclater@live.com>
2022-02-28 20:04:19 -0800

    Search and header link icons added

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,7 @@ import ChatIcon from "@mui/icons-material/Chat";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+import { Avatar } from "@mui/material";
 
 function Header() {
   return (
@@ -17,9 +18,24 @@ function Header() {
 
             <div className="flex items-center space-x-1 dark:md:bg-gray-700 pt-2.5 px-4 rounded w-full">
                 <SearchRoundedIcon/>
+                <input 
+                    type="text" 
+                    placeholder="Search" 
+                    className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow"
+                />
             </div>
         </div>
         {/* { Right } */}
+
+        <div className="flex items-center space-x-6">
+        <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
+        <HeaderLink Icon={GroupIcon} text="My Network" feed />
+        <HeaderLink Icon={BusinessCenterIcon} text="Jobs" feed hidden />
+        <HeaderLink Icon={ChatIcon} text="Messaging" feed />
+        <HeaderLink Icon={NotificationsIcon} text="Notifications" feed />
+        <HeaderLink Icon={Avatar} text="Me" feed avatar hidden />
+        <HeaderLink Icon={AppsOutlinedIcon} text="Work" feed hidden />
+        </div>
     </header>
   );
 }
