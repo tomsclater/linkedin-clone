a69f4735b3c545803011580b6751d7fe69388ddc
Thomas <tomsclater@live.com>
2022-03-11 00:31:31 -0800

    Setting up feed and input

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,12 @@
+import Input from "./Input";
+
 function Feed() {
-  return <div>Feed</div>;
+    return (
+        <div className="space-y-6 pb-24 max-w-lg">
+            <Input />
+            {/* {Posts} */}
+        </div>
+    );
 }
 
 export default Feed;
