4016632941ee2377a2153fb5d03d1fbc411ad229
Thomas <tomsclater@live.com>
2022-03-11 22:06:26 -0800

    Start a post button added with avatar

--- components/Input.js
+++ components/Input.js
@@ -1,7 +1,30 @@
+import { Avatar } from "@mui/material";
+import { useSession } from "next-auth/react";
+import { motion } from "framer-motion";
+
 function Input() {
-  return (
-  <div>Input</div>
-  );
+    const { data: session } = useSession();
+    return (
+        <div className="bg-white dark:bg-[#1D2226] rounded-lg p-3 space-y-3 border border-gray-300">
+            <div className="flex items-center space-x-2">
+                <Avatar 
+                    src={session?.user?.image} 
+                    className="!h-10 !w-10 cursor-pointer" 
+                />
+                <motion.button
+                    whileHover={{ scale: 1.01 }}
+                    whileTap={{ scale: 0.99 }}
+                    className="rounded-full border border-gray-400 py-2.5 px-3 opacity-80 hover:opacity-100 font-medium w-full text-left"
+                    onClick={() => {
+                        setModalOpen(true);
+                        setModalType("dropIn");
+                    }}
+                >
+                    Start a post
+                </motion.button>
+            </div>
+        </div>
+    );
 }
 
 export default Input;
