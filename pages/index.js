a0a3b5dce75e5ccd8d4dde658aeb5a649de7e290
Thomas <tomsclater@live.com>
2022-03-12 17:39:38 -0800

    Modal functioning

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
+import { AnimatePresence } from 'framer-motion';
 import { getSession, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { modalState, modalTypeState } from '../atoms/modalAtom';
 import Feed from '../components/Feed';
 import Header from '../components/Header';
+import Modal from '../components/Modal';
 import Sidebar from '../components/Sidebar';
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
   const router = useRouter()
   const { status } = useSession({
     required: true,
@@ -31,6 +37,11 @@ export default function Home() {
           <Feed/>
         </div>
           {/* {Widgets} */}
+          <AnimatePresence>
+          {modalOpen && (
+            <Modal handleClose={() => setModalOpen(false)} type={modalType} />
+          )}
+        </AnimatePresence>
       </main>
     </div>
   );
