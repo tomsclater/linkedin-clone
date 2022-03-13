7e85c0b4f97e81d8e183db7e2372966f972444a5
Thomas <tomsclater@live.com>
2022-03-12 21:13:24 -0800

    Finishing post functionality

--- components/Form.js
+++ components/Form.js
@@ -18,8 +18,15 @@ function Form() {
                 email: session.user.email,
                 userImg: session.user.image,
                 createdAt: new Date().toString()
-            })
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
         })
+
+        const responseData = await response.json();
+
+        setModalOpen(false);
     };
 
     const addHashtag = async (e) => {
@@ -27,32 +34,32 @@ function Form() {
         e.preventDefault();
     };
 
-  return <form className=" flex flex-col relative space-y-2 text-black/80 dark:text-white/75">
-        <textarea 
-            rows="4" 
-            placeholder="What do you want to talk about?"
-            className="bg-transparent focus:outline-none dark:placeholder-white/75"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-        />
-        <div>
-        <button
-                  className="text-blue-700 dark:text-white font-semibold rounded border border-blue-700 px-5 py-1.5 transition-all hover:border-2 inputButton"
-                  onClick={addHashtag}
-                >
-                  Add hashtag
-        </button>
-        </div>
+    return <form className=" flex flex-col relative space-y-2 text-black/80 dark:text-white/75">
+            <textarea 
+                rows="4" 
+                placeholder="What do you want to talk about?"
+                className="bg-transparent focus:outline-none dark:placeholder-white/75"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            />
+            <div>
+            <button
+                    className="text-blue-700 dark:text-white font-semibold rounded border border-blue-700 px-5 py-1.5 transition-all hover:border-2 inputButton"
+                    onClick={addHashtag}
+                    >
+                    Add hashtag
+            </button>
+            </div>
 
-        <button 
-            className="absolute bottom-0 right-0 font-medium bg-blue-400 hover:bg-blue-500 disabled:text-black/40 disabled:bg-white/75 disabled:cursor-not-allowed text-white rounded-full px-3.5 py-1"
-            type="submit"
-            onClick={uploadPost}
-            disabled={!input.trim()}
-        >
-            Post
-        </button>
-  </form>;
+            <button 
+                className="absolute bottom-0 right-0 font-medium bg-blue-400 hover:bg-blue-500 disabled:text-black/40 disabled:bg-white/75 disabled:cursor-not-allowed text-white rounded-full px-3.5 py-1"
+                type="submit"
+                onClick={uploadPost}
+                disabled={!input.trim()}
+            >
+                Post
+            </button>
+    </form>;
 }
 
 export default Form;
