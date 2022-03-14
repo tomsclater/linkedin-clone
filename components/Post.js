9f45137b65c9650fe6b084d894780d3cd45bbd75
Thomas <tomsclater@live.com>
2022-03-13 22:18:59 -0700

    Building posts

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,12 @@
-function Post({post}) {
-  return <div>
-      <img src={post.userImg} alt="" />
-      <p>{post.input}</p>
+import { Avatar } from "@mui/material";
+
+function Post({ post, modalPost }) {
+  return <div className={`bg-white dark:bg-[#1D2226] ${
+      modalPost ? "rounded-r-lg": "rounded-lg"
+  } space-y-2 py-2.5 border border-gray-300 dark:border-gray-500`}>
+      <div className="flex items-center px-2.5 cursor-pointer">
+        <Avatar src={post.userImg} className="!h-10 !w-10 cursor-pointer" />
+      </div>
   </div>;
 }
 
