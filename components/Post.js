4257d217b1d492a8247847686dcd54e563f283f8
Thomas <tomsclater@live.com>
2022-03-13 22:32:17 -0700

    Importing icons and preparing post modal action

--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,42 @@
-import { Avatar } from "@mui/material";
+import { Avatar, IconButton } from "@mui/material";
+import MoreHorizRoundedIcon from "@mui/icons-material/MoreHorizRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import ThumbUpOffAltOutlinedIcon from "@mui/icons-material/ThumbUpOffAltOutlined";
+import ThumbUpOffAltRoundedIcon from "@mui/icons-material/ThumbUpOffAltRounded";
+import { useRecoilState } from "recoil";
+import { handlePostState, getPostState } from "../atoms/postAtom";
+import { useState } from "react";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import ReplyRoundedIcon from "@mui/icons-material/ReplyRounded";
+import CommentOutlinedIcon from "@mui/icons-material/CommentOutlined";
+import { modalState, modalTypeState } from "../atoms/modalAtom";
+// import TimeAgo from "timeago-react";
+import { useSession } from "next-auth/react";
 
 function Post({ post, modalPost }) {
+    const [modalOpen, setModalOpen] = useRecoilState(modalState);
+
   return <div className={`bg-white dark:bg-[#1D2226] ${
       modalPost ? "rounded-r-lg": "rounded-lg"
   } space-y-2 py-2.5 border border-gray-300 dark:border-gray-500`}>
       <div className="flex items-center px-2.5 cursor-pointer">
         <Avatar src={post.userImg} className="!h-10 !w-10 cursor-pointer" />
+        <div className="mr-auto ml-2 leading-none">
+            <h6 className="font-medium hover:text-blue-500 hover:underline">
+                {post.username}
+            </h6>
+            <p className="text-sm dark:text-white/75 opacity-80">{post.email}</p>
+            {/* {Time ago stamp} */}
+        </div>
+        {modalPost ? (
+          <IconButton onClick={() => setModalOpen(false)}>
+            <CloseRoundedIcon className="dark:text-white/75 h-7 w-7" />
+          </IconButton>
+        ) : (
+          <IconButton>
+            <MoreHorizRoundedIcon className="dark:text-white/75 h-7 w-7" />
+          </IconButton>
+        )}
       </div>
   </div>;
 }
