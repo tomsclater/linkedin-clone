e3606f3d5b5969d2f1e5bb9e9f5e271526402486
Thomas <tomsclater@live.com>
2022-03-15 22:48:44 -0700

    Sidebar image added

--- components/Widgets.js
+++ components/Widgets.js
@@ -31,6 +31,17 @@ function Widgets({ articles }) {
         </div>
       </div>
       {/* {Ads} */}
+      <div className="bg-white dark:bg-[#1D2226] w-11/12 h-64 px-2.5 mt-2 rounded-lg sticky top-20 border border-gray-300 dark:border-none">
+        <div className="relative w-full h-full">
+            <Image
+                src="https://rb.gy/kbfeaa"
+                layout="fill"
+                objectFit="contain"
+                priority
+            />
+        </div>
+      </div>
+
   </div>
   );
 }
