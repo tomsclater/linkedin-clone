56f578fdb61d28f48ae20e554c2572c64519b5dd
Thomas <tomsclater@live.com>
2022-03-15 22:51:59 -0700

    Fixing typos

--- components/Widgets.js
+++ components/Widgets.js
@@ -5,10 +5,10 @@ import TimeAgo from "timeago-react";
 
 function Widgets({ articles }) {
   return (
-  <div classNAme="hidden xl:inline space-y-2">
+  <div className="hidden xl:inline space-y-2">
       {/* {News} */}
       <div className="bg-white dark:bg-[#1d2226] py-2.5 rounded-lg space-y-2 w-11/12 overflow-hidden border border-gray-300 dark:border-none">
-        <div className="flex items-center justify-between font-bold p-x-2.5">
+        <div className="flex items-center justify-between font-bold px-2.5">
             <h4>LinkedIn News</h4>
             <InfoRoundedIcon className="h-5 w-5" />
         </div>
@@ -31,7 +31,7 @@ function Widgets({ articles }) {
         </div>
       </div>
       {/* {Ads} */}
-      <div className="bg-white dark:bg-[#1D2226] w-11/12 h-64 px-2.5 mt-2 rounded-lg sticky top-20 border border-gray-300 dark:border-none">
+      <div className="bg-white dark:bg-[#1D2226] w-11/12 h-64 px-2.5 mt-2 rounded-lg sticky border border-gray-300 dark:border-none">
         <div className="relative w-full h-full">
             <Image
                 src="https://rb.gy/kbfeaa"
